5f5f22ae218a24150bc88fcd81572034b2aed903
toum21 <pey2moo1@gmail.com>
2023-11-07 18:04:45 -0800

    fixed errors in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,30 +1,29 @@
--- DROP DATABASE IF EXISTS employee_tracker_db;
+DROP DATABASE IF EXISTS employee_tracker_db;
 CREATE DATABASE employee_tracker_db;
 
 USE employee_tracker_db;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT ,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id) 
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT ,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id)
-    REFRENCES department(id)   
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)  
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    id INT NOT NULL AUTO_INCREMENT ,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id)
-    REFRENCES role(id)
-    ON DELETE SET NULL
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
 );
